ac49804cf6ee3e7b9c511569383d3fd448a965c9
Jacob Gadikian <jacobgadikian@gmail.com>
2025-05-20 17:42:39 +0700

    Align malachite rev for tests

--- aura-node-lib/tests/app_message_loop.rs
+++ aura-node-lib/tests/app_message_loop.rs
@@ -1,16 +1,16 @@
-use std::sync::{Arc, Mutex};
 use aura_node_lib::AuraState;
 use aura_node_lib::node::app_message_loop;
-use malachitebft_app_channel::{Channels, AppMsg};
-use malachitebft_test::{
-    TestContext, ValidatorSet as TestValidatorSet, Address as TestAddress,
-    Ed25519Provider, Height as TestHeight, Validator,
-};
-use malachitebft_signing_ed25519::PrivateKey as Ed25519PrivateKey;
+use malachitebft_app_channel::{AppMsg, Channels};
 use malachitebft_core_types::VotingPower;
-use tokio::sync::{mpsc, oneshot};
-use rand::thread_rng;
 use malachitebft_engine::util::events::TxEvent;
+use malachitebft_signing_ed25519::PrivateKey as Ed25519PrivateKey;
+use malachitebft_test::{
+    Address as TestAddress, Ed25519Provider, Height as TestHeight, TestContext, Validator,
+    ValidatorSet as TestValidatorSet,
+};
+use rand::thread_rng;
+use std::sync::{Arc, Mutex};
+use tokio::sync::{mpsc, oneshot};
 
 #[tokio::test]
 async fn test_get_history_min_height() {
@@ -24,19 +24,33 @@ async fn test_get_history_min_height() {
     let (net_tx, _net_rx) = mpsc::channel(4);
     let event_tx = TxEvent::new();
 
-    let channels = Channels { consensus: cons_rx, network: net_tx, events: event_tx };
+    let channels = Channels {
+        consensus: cons_rx,
+        network: net_tx,
+        events: event_tx,
+    };
     let ctx = TestContext::default();
 
-    let sk = Ed25519PrivateKey::generate(&mut thread_rng());
+    let sk = Ed25519PrivateKey::generate(thread_rng());
     let validator = Validator::new(sk.public_key(), 1 as VotingPower);
     let validators = TestValidatorSet::new(vec![validator]);
     let signing = Ed25519Provider::new(sk);
     let addr = TestAddress::from_public_key(&signing.private_key().public_key());
 
-    let handle = tokio::spawn(app_message_loop(state_arc.clone(), ctx, channels, validators.clone(), signing, addr));
+    let handle = tokio::spawn(app_message_loop(
+        state_arc.clone(),
+        ctx,
+        channels,
+        validators.clone(),
+        signing,
+        addr,
+    ));
 
     let (reply_tx, reply_rx) = oneshot::channel();
-    cons_tx.send(AppMsg::GetHistoryMinHeight { reply: reply_tx }).await.unwrap();
+    cons_tx
+        .send(AppMsg::GetHistoryMinHeight { reply: reply_tx })
+        .await
+        .unwrap();
     let res = reply_rx.await.unwrap();
     assert_eq!(res, TestHeight::new(0));
 
